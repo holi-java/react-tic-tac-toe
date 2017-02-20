8fe0bf73ed95f48f2902d9d44df11f97d8deaf71
holi <32414154@qq.com>
2017-02-20 20:43:09 +0800

    create tic-tac-toe project

---
+++ src/env.test.js
@@ -0,0 +1,30 @@
+test('environment is ok', () => {
+    expect(true).toBe(true);
+});
+
+test('generator supported', () => {
+    class Foo {
+        *[Symbol.iterator]() {
+            yield * [1, 2, 3];
+        }
+    }
+
+    let foo = new Foo();
+    expect([...foo]).toEqual([1, 2, 3]);
+});
+
+
+test('async function supported', (done) => {
+    function it(v) {
+        return v;
+    }
+
+    async function itr() {
+        return [await it(1), await it(2)];
+    }
+
+    itr().then((result) => {
+        expect(result).toEqual([1, 2]);
+        done();
+    });
+});
